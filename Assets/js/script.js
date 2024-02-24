cd7f2b3f96d2d02be120e422aa7d92b7161c2412
MtechWonders <mtechwonders@gmail.com>
2024-02-24 15:19:47 +0100

    update

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,5 @@
-let openHam = document.querySelector('#openHam');
-let closeHam = document.querySelector('#closeHam');
-let navigationItems = document.querySelector('#navigation-items');
-
-const hamburgerEvent = (navigation, close, open) => {
-    navigationItems.style.display = navigation;
-    closeHam.style.display = close;
-    openHam.style.display = open;
-};
-
-openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
-closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".dropbtn").addEventListener("click", function() {
+    document.getElementById("dropdownContent").classList.toggle("show");
+  });
+});
